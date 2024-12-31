f06d92a5f4cd556ee0d4fbac0609091b313165fa
Ruisu <joseluisosorioguzman11@gmail.com>
2024-12-30 20:26:25 -0500

    Menu Animation sin Animation xD

--- script.js
+++ script.js
@@ -3,13 +3,15 @@ const btnCerrar = document.getElementById("btn-cerrar");
 const navMenu = document.getElementById("nav-menu");
 
 btnAbrir.addEventListener("click", () => {
+  navMenu.style.transition = ".5s";
+  navMenu.style.opacity = "1";
   navMenu.style.display = "flex";
   btnAbrir.style.display = "none";
   btnCerrar.style.display = "block";
 });
 
 btnCerrar.addEventListener("click", () => {
-  navMenu.style.display = "none";
+  navMenu.style.opacity = "0";
   btnAbrir.style.display = "block";
   btnCerrar.style.display = "none";
 });
